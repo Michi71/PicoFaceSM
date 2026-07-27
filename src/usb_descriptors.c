b38d6ff9264ecb774610e41930b4fe27f70097e2
Michi71 <47889385+Michi71@users.noreply.github.com>
2026-07-27 19:44:16 +0200

    Change product name from 'PicoFaceRD' to 'PicoFaceSM'

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -134,7 +134,7 @@ char const* string_desc_arr [] =
 {
   (const char[]) { 0x09, 0x04 }, 	// 0: is supported language is English (0x0409)
   "MIDICompany",                 	// 1: Manufacturer
-  "PicoFaceRD",             // 2: Product
+  "PicoFaceSM",             // 2: Product
   usb_serial,                 		// 3: Serials, should use chip ID
 };
 
